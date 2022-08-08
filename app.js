92ce9ea3258f55fe60cb1890bed205061363c3d6
brittanydianah.github.io <brittanydianah@gmail.com>
2022-08-08 12:53:17 -0400

    added guess1 functionality

--- app.js
+++ app.js
@@ -1,27 +1,118 @@
 const words = ["redux", "query", "coder", "pixel", "array", "loops", "react"];
-let secretWord = "";
-let regex = /[a-zA-Z]/ig
 const guess1 = document.querySelector("#guess1").children;
-const guess2 = document.querySelector("#guess2");
-const guess3 = document.querySelector("#guess3");
-const guess4 = document.querySelector("#guess4");
-const guess5 = document.querySelector("#guess5");
-const guess6 = document.querySelector("#guess6");
+const guess2 = document.querySelector("#guess2").children;
+const guess3 = document.querySelector("#guess3").children;
+const guess4 = document.querySelector("#guess4").children;
+const guess5 = document.querySelector("#guess5").children;
+const guess6 = document.querySelector("#guess6").children;
 
 
-if (regex.test(guess1[0].value) == true){
-    console.log(guess1[0].value)
-}else{console.log(false)}
 
-for (let i=0; i<words.length; i++){
-  let secretWord = words[i];
+// for(let i=0; i<6; i++){
+// guess2[i].disabled = true;
+// guess3[i].disabled = true;
+// guess4[i].disabled = true;
+// guess5[i].disabled = true;
+// guess6[i].disabled = true;
+// }
+
+guess2[0].disabled = true;
+guess2[1].disabled = true;
+guess2[2].disabled = true;
+guess2[3].disabled = true;
+guess2[4].disabled = true;
+
+guess3[0].disabled = true;
+guess3[1].disabled = true;
+guess3[2].disabled = true;
+guess3[3].disabled = true;
+guess3[4].disabled = true;
+
+guess4[0].disabled = true;
+guess4[1].disabled = true;
+guess4[2].disabled = true;
+guess4[3].disabled = true;
+guess4[4].disabled = true;
+
+guess5[0].disabled = true;
+guess5[1].disabled = true;
+guess5[2].disabled = true;
+guess5[3].disabled = true;
+guess5[4].disabled = true;
+
+guess6[0].disabled = true;
+guess6[1].disabled = true;
+guess6[2].disabled = true;
+guess6[3].disabled = true;
+guess6[4].disabled = true;
+
+document.addEventListener("keyup", function(e){
+  let secretWord = words[0].split("");
+
+      if(guess1[0].value !=="" && guess1[1].value !=="" && guess1[2].value !=="" && guess1[3].value !=="" && guess1[4].value !=="" && e.key =="Enter"){
+         
+
+              if(guess1[0].value ==secretWord[0]){
+                guess1[0].style.backgroundColor = "green";
+              }
+              if(guess1[1].value ==secretWord[1]){
+                guess1[1].style.backgroundColor = "green";
+              }
+              if(guess1[2].value ==secretWord[2]){
+                guess1[2].style.backgroundColor = "green";
+              }
+              if(guess1[3].value ==secretWord[3]){
+                guess1[3].style.backgroundColor = "green";
+              }
+              if(guess1[4].value ==secretWord[4]){
+                guess1[4].style.backgroundColor = "green";
+              }
+
+              if((guess1[0].value !==secretWord[0]) && (guess1[0].value ==secretWord[1] || guess1[0].value ==secretWord[2] || guess1[0].value ==secretWord[3] || guess1[0].value ==secretWord[4]) ){
+                guess1[0].style.backgroundColor = "yellow";
+              }
+        
+              if((guess1[1].value !==secretWord[1]) && (guess1[1].value ==secretWord[0] || guess1[1].value ==secretWord[2] || guess1[1].value ==secretWord[3] || guess1[1].value ==secretWord[4]) ){
+                guess1[1].style.backgroundColor = "yellow";
+              }
+
+              if((guess1[2].value !==secretWord[2]) && (guess1[2].value ==secretWord[1] || guess1[2].value ==secretWord[0] || guess1[2].value ==secretWord[3] || guess1[2].value ==secretWord[4]) ){
+                guess1[2].style.backgroundColor = "yellow";
+              }
+
+              if((guess1[3].value !==secretWord[3]) && (guess1[3].value ==secretWord[1] || guess1[3].value ==secretWord[2] || guess1[3].value ==secretWord[0] || guess1[3].value ==secretWord[4]) ){
+                guess1[3].style.backgroundColor = "yellow";
+              }
+
+              if((guess1[4].value !==secretWord[4]) && (guess1[4].value ==secretWord[1] || guess1[4].value ==secretWord[2] || guess1[4].value ==secretWord[3] || guess1[4].value ==secretWord[0]) ){
+                guess1[4].style.backgroundColor = "yellow";
+              }
+
+              guess1[0].disabled = true;
+              guess1[1].disabled = true;
+              guess1[2].disabled = true;
+              guess1[3].disabled = true;
+              guess1[4].disabled = true;
+
+              guess2[0].disabled = false;
+              guess2[1].disabled = false;
+              guess2[2].disabled = false;
+              guess2[3].disabled = false;
+              guess2[4].disabled = false;
+          
+      }
+
+
+
+})
+
+
   
+   
+
   
 
 
-
-
-}
 
 
 
